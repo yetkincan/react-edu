b2a39c0124859457f69a1478fcf0427bc5a986d1
yetkin <yetkin@yetkins-MacBook-Pro.local>
2019-04-01 14:01:49 +0300

    webpack babel lint

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { render } from 'react-dom'
+import React from 'react';
+import { render } from 'react-dom';
 
 /*
 // MyModule
@@ -15,7 +15,9 @@ const MyTitle = function(props) {
 };
 const MyFirstComponent = function() {
   //return React.createElement('div', null, React.createElement('h1', null, 'This is my first component!'))
-  return React.createElement('div', null, 
+  return React.createElement(
+    'div',
+    null,
     React.createElement(MyTitle, {
       title: 'Game of Thrones',
       color: 'YellowGreen'
@@ -28,8 +30,8 @@ const MyFirstComponent = function() {
       title: 'Rick and Morty',
       color: 'LimeGreen'
     }),
-
-    React.createElement(MyTitle, { title: 'Silicon Valley', color: 'peru' })
+    React.createElement(MyTitle, { title: 'Silicon Valley', color: 'peru' }),
+    React.createElement(MyTitle, { title: 'House of Cards', color: 'peru' })
   );
 };
 /*
@@ -37,12 +39,13 @@ var a = 4;
 a = a + x;
 */
 render(React.createElement(MyFirstComponent), document.getElementById('app'));
-
+/*
 //yarn global add prettier
 //prettier --single-quote --print-width=120 ClientApp.jsx
-//prettier --single-quote --print-width=120 --write ClientApp.jsx
+//prettier --single-quote --print-width=120 --write ClientApp.jsx 
+*/
 // "format":"prettier --write --single-quote --print-width=120 --parser=flow --tab-width=2 \"js/**/*.{js,jsx}\""
-
+/*
 //yarn add eslint
 // yarn add eslint-plugin-prettier
 // yarn add eslint-config-prettier
@@ -52,6 +55,12 @@ render(React.createElement(MyFirstComponent), document.getElementById('app'));
 // yarn add eslint-plugin-react
 // yarn add eslint-plugin-import
 // vscode'u application folderına koyup orada açmak lazım
-//eslint **/*.{js,jsx} --quiet
+*/
+// eslint **/*.{js,jsx} --quiet
 
-//./node_modules/.bin/webpack js/ClientApp.js public/bundle.js
+// ./node_modules/.bin/webpack js/ClientApp.js public/bundle.js
+// yarn add babel-preset-es2015
+// ./node_modules/.bin/webpack
+// yarn build
+// yarn build -- -p   => sourcemapleri çıkarır
+// yarn build -- --watch  => her saveden sonra tekrar paketler
