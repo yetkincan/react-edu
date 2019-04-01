e453d8c508b59d3982f42e43c18ff5ae84cffddd
yetkin <yetkin@yetkins-MacBook-Pro.local>
2019-04-01 22:39:09 +0300

    loading json data

--- js/Search.js
+++ js/Search.js
@@ -1,5 +1,11 @@
 import React from 'react'
+import preload from '../data.json'
 
-const Search = () => <h1>hi lol this is search</h1>
+const Search = () => (
+    <div className="search">
+       {/* <pre><code>{JSON.stringify(preload, null, 4)}</code>0</pre> */}
+       {preload.shows.map( (show) => <h3>{show.title}</h3> )}
+    </div>
+)
 
 export default Search
