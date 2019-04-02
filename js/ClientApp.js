cb8774d8cab69cdc91dfa79874756be7307458e4
yetkin <yetkin@yetkins-MacBook-Pro.local>
2019-04-02 19:22:33 +0300

    managing state

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
-import  { BrowserRouter, Route, Switch } from 'react-router-dom'
-import Landing from './Landing'
-import Search from './Search'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Landing from './Landing';
+import Search from './Search';
 
-const FourOhFour = () => <h1>404</h1>
+const FourOhFour = () => <h1>404</h1>;
 
 const App = () => (
   <BrowserRouter>
     <div className="app">
-    <Switch>
+      <Switch>
         <Route exact path="/" component={Landing} />
         <Route path="/search" component={Search} />
         <Route component={FourOhFour} />
       </Switch>
     </div>
   </BrowserRouter>
-    
-  )
+);
 
-render(<App />, document.getElementById("app"))
+render(<App />, document.getElementById('app'));
