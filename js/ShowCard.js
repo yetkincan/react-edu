af1be02a7717f63e2e5a4a2b0da179e97b62da23
yetkin <yetkin@yetkins-MacBook-Pro.local>
2019-04-02 15:19:28 +0300

    spreadoperator and keyprop

--- js/ShowCard.js
+++ js/ShowCard.js
@@ -1,24 +1,22 @@
 import React from 'react'
-import { shape, string } from 'prop-types'
+import { string } from 'prop-types'
 
 const ShowCard = props => (
     <div className='show-card'>
-        <img alt={`${props.show.title} Show Poster`} src={`/public/img/posters/${props.show.poster}`} />
+        <img alt={`${props.title} Show Poster`} src={`/public/img/posters/${props.poster}`} />
         <div>
-            <h3>{props.show.title}</h3>
-            <h4>{props.show.year}</h4>
-            <p>{props.show.description}</p>
+            <h3>{props.title}</h3>
+            <h4>{props.year}</h4>
+            <p>{props.description}</p>
         </div>
     </div>
 )
 
 ShowCard.propTypes = {
-    show: shape({
-        poster: string.isRequired,
-        title: string.isRequired,
-        year: string.isRequired,
-        description: string.isRequired
-    }).isRequired
+    poster: string.isRequired,
+    title: string.isRequired,
+    year: string.isRequired,
+    description: string.isRequired
 }
 
 export default ShowCard
