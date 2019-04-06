28ea32e79b1735208dc3ddbe8050a50eedb57d08
yetkin <yetkin@yetkins-MacBook-Pro.local>
2019-04-06 20:40:48 +0300

    shallow testing with enzyme

--- __tests__/Search.spec.js
+++ __tests__/Search.spec.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
+import { shallow } from 'enzyme'
 import Search from '../js/Search'
-
+console.log(process.env.NODE_ENV)
 test('Search renders correctly', () => {
-    const component = renderer.create(<Search />)
-    const tree = component.toJSON
-    expect(tree).toMatchSnapshot()
+    const component = shallow(<Search />)
+    expect(component).toMatchSnapshot()
 })
