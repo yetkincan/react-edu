562d059d2f0591189cb7d809af78189b7f22fb85
yetkin <yetkin@yetkins-MacBook-Pro.local>
2019-04-10 18:18:48 +0300

    Finishing the detail component

--- js/Details.js
+++ js/Details.js
@@ -2,11 +2,32 @@
 
 import React from 'react';
 
-const Details = (props: any) => (
-  <div className="details">
-    {/*<pre><code>{JSON.stringify(props, null, 4)}</code></pre>*/}
-    <pre><code>{JSON.stringify(props, null, 4)}</code></pre>
-  </div>
-);
+const Details = (props: { show: Show }) => {
+  const { title, description, year, poster, trailer } = props.show;
+  return (
+    <div className="details">
+      <header>
+        <h1>svideo</h1>
+      </header>
+      <section>
+        <h1>{title}</h1>
+        <h2>({year})</h2>
+        <img src={`/public/img/posters/${poster}`} alt={`Poster for ${title}`} />
+        <p>{description}</p>
+      </section>
+      <div>
+        <iframe
+          id="ytplayer"
+          type="text/html"
+          width="640"
+          height="360"
+          src={`http://www.youtube.com/embed/${trailer}?autoplay=0;showinfo=0`}
+          title={`Trailer for ${title}`}
+          frameBorder="0"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Details;
