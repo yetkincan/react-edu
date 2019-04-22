b373d62c3f1a39f8a102bb2f5c0ba848d586828a
yetkin <yetkin@yetkins-MacBook-Pro.local>
2019-04-22 17:02:28 +0300

    react perf tools

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -2,7 +2,17 @@
 
 import React from 'react';
 import { render } from 'react-dom';
+import Perf from 'react-addons-perf';
 import App from './App';
+
+window.Perf = Perf;
+Perf.start();
+/*
+Perf.stop();
+Perf.printWasted();
+Perf.printInclusive();
+Perf.printExclusive();
+*/
 
 const renderApp = () => {
   render(<App />, document.getElementById('app'));
